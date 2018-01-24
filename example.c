24da1117e952de044214ab2ffaa3acc7eefaa44c
kablaa <kablaa@hackmy.world>
2018-01-24 10:48:50 -0800

    fin

--- example.c
+++ example.c
@@ -4,10 +4,9 @@
 #include <string.h>
 #define MAX_NAME_LEN 20
 #define MAX_BOOK_TILE_LEN 100
+#define MAX_NUM_USERS 10
 
 
-//globals
-int g_numUsers=0;
 
 //structures
 typedef struct Book{
@@ -26,18 +25,52 @@ User* createUser();
 void printUser(User *user);
 void printMenue();
 Book *createBook();
+void clearstdin();
+
+//globals
+int g_numUsers=0;
 
 int main(int argc, const char *argv[])
 {
-    User * newUser = createUser();
-    printUser(newUser);
+    char selection;
+    int id = 0;
+    User *userList[MAX_NUM_USERS];
+    while(selection != 'q'){
+    printMenue();
+    selection = getchar();
+    clearstdin();
+        if(selection == 'a'){
+            if(g_numUsers < MAX_NUM_USERS){
+                User * newUser = createUser();
+                userList[g_numUsers] = newUser;
+                puts("----------------------------------");
+                puts("new user added to database : ");
+                printUser(newUser);
+            }else
+                puts("database is at capacity");
+        }else if(selection == 'l'){
+            puts("enter id: ");
+            id = getchar() - '0';
+            clearstdin();
+            if(id > 0 && id <= g_numUsers )
+                printUser(userList[id ]);
+            else
+                puts("invalid user id");
+        }
+    }
     
     return 0;
 }
+void clearstdin(){
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) { }
+}
 void printMenue(){
-    puts("a: add new user");
+    puts("----------------------------------");
+    puts("a: add new user (max of 10 users)");
     puts("l: list user by id");
     puts("q: Quit");
+    puts("----------------------------------");
 }
 
 Book *createBook(){
@@ -50,14 +83,15 @@ Book *createBook(){
 
     puts("how many pages?");
     scanf("%d",&newBook -> numPages);
+    clearstdin();
     return newBook;
 }
 User* createUser() {
     printf("user name (max len %d): ",MAX_NAME_LEN);
     fflush(stdout);
     User *newUser = malloc(sizeof(User));
-    newUser -> id = g_numUsers;
     g_numUsers +=1;
+    newUser -> id = g_numUsers;
     newUser->name = malloc(sizeof(char)*MAX_NAME_LEN + 1);
     memset(newUser ->name, 0, MAX_NAME_LEN);
     read(0,newUser -> name, MAX_NAME_LEN);
